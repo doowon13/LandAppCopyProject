fa168f25ad9c210895cc7f0d8dcf0d2d529aac23
doowon13 <endnjs2007@gmail.com>
2021-05-25 12:39:31 +0900

    방 목록 어댑터의 뼈대작업

---
+++ app/src/main/java/com/pluto/landappcopyproject/adapters/RoomAdapter.kt
@@ -0,0 +1,30 @@
+package com.pluto.landappcopyproject.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.pluto.landappcopyproject.R
+import com.pluto.landappcopyproject.datas.Room
+import java.math.MathContext
+
+class RoomAdapter(
+    val mContext : Context,
+    val resId : Int,
+    val mList : ArrayList<Room> ) : ArrayAdapter<Room>(mContext, resId, mList) {
+
+    val inflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = inflater.inflate(R.layout.room_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
+}

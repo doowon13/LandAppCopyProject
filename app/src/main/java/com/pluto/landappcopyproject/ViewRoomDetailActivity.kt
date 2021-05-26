e3d563c3415506fa17b8a351d3fe980d9ecabf9b
doowon13 <endnjs2007@gmail.com>
2021-05-26 12:48:36 +0900

    상세정보화면에 대한 UI추가

---
+++ app/src/main/java/com/pluto/landappcopyproject/ViewRoomDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.pluto.landappcopyproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewRoomDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_room_detail)
+    }
+}

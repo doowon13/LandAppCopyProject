14b73d120cf586eedb4097d8cde45990b648e8db
doowon13 <endnjs2007@gmail.com>
2021-05-26 13:14:54 +0900

    방 상세정보로 데이터를 넘겨서 반영

--- app/src/main/java/com/pluto/landappcopyproject/ViewRoomDetailActivity.kt
+++ app/src/main/java/com/pluto/landappcopyproject/ViewRoomDetailActivity.kt
@@ -2,10 +2,21 @@ package com.pluto.landappcopyproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pluto.landappcopyproject.datas.Room
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_view_room_detail.*
 
 class ViewRoomDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_room_detail)
+
+        val roomData = intent.getSerializableExtra("room") as Room
+
+        priceTxt.text = roomData.getFormattedPrice()
+        descriptionTxt.text = roomData.description
+        addressTxt.text = roomData.address
+        floorTxt.text = roomData.getFormattedFloor()
+
     }
 }
